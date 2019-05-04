d8387eaf0d2d99001673aa82181f058117e11d23
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-05-04 13:04:41 -0500

    Initial commit

---
+++ Hackathon/Hackathon.Models/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackathon.Models.Model
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
